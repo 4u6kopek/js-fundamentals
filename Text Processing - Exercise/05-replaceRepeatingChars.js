da531bc54477af2f0f52c2374c89b31997c33baf
Bogomil Dimitrov <92969096+4u6kopek@users.noreply.github.com>
2024-07-12 21:10:57 +0300

    Add files via upload

---
+++ Text Processing - Exercise/05-replaceRepeatingChars.js
@@ -0,0 +1,13 @@
+function replaceRepeatingChars(str) {
+    let nonRepeatedChars = '';
+    let currentLastChar = '';
+
+    for (const char of str) {
+        if (char !== currentLastChar) {
+            nonRepeatedChars += char;
+            currentLastChar = char;
+        }
+    }
+    console.log(nonRepeatedChars);
+}
+replaceRepeatingChars('aaaaaaaaaaabdees');
