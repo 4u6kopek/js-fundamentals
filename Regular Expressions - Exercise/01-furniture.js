fd6aac32d72db86a9b3925d706d94956f7585e75
Bogomil Dimitrov <92969096+4u6kopek@users.noreply.github.com>
2024-07-19 19:06:59 +0300

    regEx02

---
+++ Regular Expressions - Exercise/01-furniture.js
@@ -0,0 +1,26 @@
+function furniture(input) {
+    const pattern = />>(?<name>[A-Za-z]+)<<(?<price>\d+\.?\d*)!(?<quantity>\d+)/;
+    let totalPrice = 0;
+    let items = [];
+
+    let command = input.shift();
+
+    while (command !== 'Purchase') {
+        let match = command.match(pattern);
+
+        if (match) {
+            let { name, price, quantity } = match.groups;
+            items.push(name);
+
+            let furniturePrice = Number(price) * Number(quantity);
+            totalPrice += furniturePrice;
+        }
+        command = input.shift();
+    }
+
+    console.log("Bought furniture:");
+    items.forEach(item => console.log(item));
+    console.log(`Total money spend: ${totalPrice.toFixed(2)}`);
+}
+
+furniture(['>>Sofa<<312.23!3', '>>TV<<300!5', '>Invalid<<!5', 'Purchase']);
