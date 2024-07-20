4cf919e45583ff78a21e76dffff416752e0dfe86
Bogomil Dimitrov <92969096+4u6kopek@users.noreply.github.com>
2024-07-21 00:25:49 +0300

    Add files via upload

---
+++ Data Types and Variables - Exercise/07-lowerOrUpper.js
@@ -0,0 +1,13 @@
+function lowerOrUpper(char) {
+  // condition ? code if true : code if false
+
+  char === char.toLowerCase() ? console.log("lower-case") : console.log("upper-case");
+
+  //   if (char === char.toLowerCase()) {
+  //     console.log('lower-case');
+  //   } else {
+  //     console.log('upper-case');
+  //   }
+}
+lowerOrUpper("L");
+lowerOrUpper("f");
