a8a392cb2bb1dfc6a5337b991a63800c36840d03
Bogomil Dimitrov <92969096+4u6kopek@users.noreply.github.com>
2024-07-22 01:07:28 +0300

    Add files via upload

---
+++ Arrays - Lab/01-sumFirstandLastArrayElements.js
@@ -0,0 +1,8 @@
+function sumFirstAndLastElement(numbers) {
+    const lastElement = numbers[numbers.length - 1];
+    const firstElement = numbers[0];
+    console.log(firstElement + lastElement);
+    
+}
+sumFirstAndLastElement([20, 30, 40]);
+sumFirstAndLastElement([5, 15]);
