1f6641579ab71f0f874faa9e909a358ec9ed4cb6
Bogomil Dimitrov <92969096+4u6kopek@users.noreply.github.com>
2024-07-23 22:40:29 +0300

    Add files via upload

---
+++ Functions - Lab/06-signCheck.js
@@ -0,0 +1,14 @@
+function signCheck(numOne, numTwo, numThree) {
+  let negativeCount = 0;
+
+  if (numOne < 0) negativeCount++;
+  if (numTwo < 0) negativeCount++;
+  if (numThree < 0) negativeCount++;
+
+  if (negativeCount === 1 || negativeCount === 3) {
+    console.log("Negative");
+  } else {
+    console.log("Positive");
+  }
+}
+signCheck(5, 12, -15);
