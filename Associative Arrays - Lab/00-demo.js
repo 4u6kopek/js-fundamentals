7ff81938f326cd3564a4c06199246189e865e420
Bogomil Dimitrov <92969096+4u6kopek@users.noreply.github.com>
2024-07-31 22:29:55 +0300

    Add files via upload

---
+++ Associative Arrays - Lab/00-demo.js
@@ -0,0 +1,8 @@
+const phoneBook = {
+    'Asen Petrov': '0876 66 66 22',
+    'Boris Marinov': '0876 66 66 32',
+    "Petur Ivanov": "0899 66 66 32",
+    "Zoya Kirilova": "0899 66 55 55",
+};
+
+console.log(phoneBook["Asen Petrov"]);
